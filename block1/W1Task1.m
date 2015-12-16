73f0c8b12525c29072defc24ab64ef085bdb9157
olaia <olaia.artieda@gmail.com>
2015-12-16 22:46:02 +0100

    Added changes in test forlder path

--- block1/W1Task1.m
+++ block1/W1Task1.m
@@ -1,27 +1,32 @@
-%
-% WEEK 1 -> Task 1
-%
-
-% Path to Dataset Images
-DirDS = 'dataset/baseline/highway/input/';
 % Path to images from dataset
-DirGT = 'dataset/baseline/highway/groundtruth/';
-% Path to the results of Test A
-DirTA = 'results_testAB_changedetection/testA/';
-% Path to the results of Test B
-DirTB = 'results_testAB_changedetection/testB/';
+DirGT = 'highway/groundtruth/';
+% Path to the results of the tests
+DirTATB = 'results/';
 
 % Load a list of the files within the folders
-ImDS = ListFiles( DirDS );
 ImGT = ListFiles( DirGT );
-ImTA = ListFiles( DirTA );
-ImTB = ListFiles( DirTB );
+ImTATB = ListFiles( DirTATB );
 
 % Count the number of files
-NumDS = numel(ImDS);
 NumGT = numel(ImGT);
+NumTATB = numel(ImTATB);
+
+%% Obtain filenames
+ImTA = [];
+ImTB = [];
+for i = 1:NumTATB
+    name = ImTATB(i).name;
+    if strncmp('test_A', name, 6) == 1
+        ImTA = [ImTA; cellstr(name)];
+    end
+    if strncmp('test_B', name, 6) == 1
+        ImTB = [ImTB; cellstr(name)];
+    end
+end
+
 NumTA = numel(ImTA);
 NumTB = numel(ImTB);
+%%
 
 % Print Info
 Message = sprintf('Number of images in TA : %d',NumTA);
@@ -35,12 +40,11 @@ disp(Message);
 A_TP = 0; A_FP = 0; A_FN = 0; A_TN = 0;
 B_TP = 0; B_FP = 0; B_FN = 0; B_TN = 0;
 
-for index=1:size(ImGT,1)
+for index=1:size(ImTA,1)
     % Load frames
-    im = double(imread(strcat(DirDS,ImDS(index).name)))/255;
-    im_gt = double(imread(strcat(DirGT,ImGT(index).name)))/255;
-    im_ta = double(imread(strcat(DirTA,ImTA(index).name)));
-    im_tb = double(imread(strcat(DirTB,ImTB(index).name)));
+    im_gt = double(imread(strcat(DirGT,ImGT(index+1200).name)))/255;
+    im_ta = double(imread(strcat(DirTATB,char(ImTA(index)))));
+    im_tb = double(imread(strcat(DirTATB,char(ImTB(index)))));
     
     % Perform comparison between test A and ground truth
     [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(im_ta, im_gt);
@@ -55,18 +59,18 @@ end
 
 
 % Evaluation on test A performance
-[A_Pre, A_Rec, A_Spe, A_Sen, A_F1] = PerformanceEvaluationPixel(A_TP, A_FP, A_FN, A_TN);
+[A_Pre, A_Rec, ~, ~, A_F1] = PerformanceEvaluationPixel(A_TP, A_FP, A_FN, A_TN);
 
 % Evaluation on test B performance
-[B_Pre, B_Rec, B_Spe, B_Sen, B_F1] = PerformanceEvaluationPixel(B_TP, B_FP, B_FN, B_TN);
+[B_Pre, B_Rec, ~, ~, B_F1] = PerformanceEvaluationPixel(B_TP, B_FP, B_FN, B_TN);
 
 % Show all results
-PixAccumEval = [A_TP, A_FP, A_FN, A_TN; B_TP, B_FP, B_FN, B_TN];
+PixAccumEval = [A_FN, A_TN, A_FP, A_TP; B_FN, B_TN, B_FP, B_TP];
 YAxis = 'Test_A Test_B';
-XAxis = 'TP  FP  FN  TN';
+XAxis = 'FN  TN  FP  TP';
 printmat(PixAccumEval,'Pixel Accumulation',YAxis, XAxis);
 
-PixPerfEval = [A_Pre, A_Rec, A_Spe, A_Sen, A_F1 ; B_Pre, B_Rec, B_Spe, B_Sen, B_F1];
+PixPerfEval = [A_Pre, A_Rec, A_F1 ; B_Pre, B_Rec, B_F1];
 YAxis = 'Test_A Test_B';
-XAxis = 'Pre  Rec  Spe  Sen F1';
+XAxis = 'Pre  Rec  F1';
 printmat(PixPerfEval,'Pixel Performance',YAxis, XAxis);
