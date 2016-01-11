ce10e07f635d9afb891d08719762c4aaae457fd4
gombru <raulgombru@gmail.com>
2016-01-11 12:57:52 +0100

    Main simplified working

--- block3/W2Task6.m
+++ block3/W2Task6.m
@@ -27,25 +27,22 @@ seqFini = 1350;
 
 for j=1:numel(numGaussians)
     %(TASK 0) Only S&G -AUC with different num of Gaussians-
-%     [hw_pre(j), hw_rec(j), hw_f1(i,k,j)] = StaufferGrimson(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
+%     [hw_pre(j), hw_rec(j), hw_f1(j)] = StaufferGrimson(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
     %(TASK 0+1) S&G + fill holes -AUC with different num of Gaussians-
-%     [hw_pre(j), hw_rec(j), hw_f1(1,1,j)] = W3Task1(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
+%     [hw_pre(j), hw_rec(j), hw_f1(j)] = W3Task1(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
 end
 
  %(TASK 0+1+2) S&G + fill holes + area filtering -AUC with different num of minPixels pixels per conected component
  numGaussians = 3;
  minPixels = 2:2:20;
 for j=1:numel(minPixels)
-    [hw_pre(j), hw_rec(j), hw_f1(1,1,j)] = W3Task2(minPixels(j), DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians, minBkgRatio, learningRate);
+    [hw_pre(j), hw_rec(j), hw_f1(j)] = W3Task2(minPixels(j), DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians, minBkgRatio, learningRate);
 end
 
-% Find the maximum f-1 score configuration
-[i,k,j] = find(hw_f1==max(max(max(hw_f1))));
-msg = sprintf('Best F1 score : %.3f (#Gauss = %d , MinBackgroundRatio = %.3f, LeraningRate = %.3f', hw_f1(i,k,j), numGaussians(j), minBkgRatio, learningRate);
-disp(msg);
 
+% AUC
 %Compute area under the Precicion-Recall Curve
-hw_AUC = trapz(pre,rec);
+hw_AUC = trapz(hw_pre,hw_rec);
 
 %% FALL Dataset
 %
@@ -63,29 +60,24 @@ learningRate = [0.014];
 seqIni = 1460;
 seqFini = 1560;
 
-
-
 for j=1:numel(numGaussians)
     %(TASK 0) Only S&G -AUC with different num of Gaussians-
-%     [fall_pre(j), fall_rec(j), fall_f1(i,k,j)] = StaufferGrimson(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
+%     [fall_pre(j), fall_rec(j), fall_f1(j)] = StaufferGrimson(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
     %(TASK 0+1) S&G + fill holes -AUC with different num of Gaussians-
-%     [fall_pre(j), fall_rec(j), fall_f1(1,1,j)] = W3Task1(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
+%     [fall_pre(j), fall_rec(j), fall_f1(j)] = W3Task1(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
 end
 
  %(TASK 0+1+2) S&G + fill holes + area filtering -AUC with different num of minPixels pixels per conected component
  numGaussians = 3;
  minPixels = 2:2:20;
 for j=1:numel(minPixels)
-    [fall_pre(j), fall_rec(j), fall_f1(1,1,j)] = W3Task2(minPixels(j), DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians, minBkgRatio, learningRate);
+    [fall_pre(j), fall_rec(j), fall_f1(j)] = W3Task2(minPixels(j), DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians, minBkgRatio, learningRate);
 end
 
-% Find the maximum f-1 score configuration
-[i,k,j] = find(fall_f1==max(max(max(fall_f1))));
-msg = sprintf('Best F1 score : %.3f (#Gauss = %d , MinBackgroundRatio = %.3f, LeraningRate = %.3f', fall_f1(i,k,j), numGaussians(j), minBkgRatio(i), learningRate(k));
-disp(msg);
 
+% AUC
 %Compute area under the Precicion-Recall Curve
-AUCFall = trapz(pre,rec);
+fall_AUC = trapz(fall_pre,fall_rec)
 
 %% TRAFFIC Dataset
 %
@@ -106,45 +98,25 @@ seqFini = 1050;
 
 for j=1:numel(numGaussians)
     %(TASK 0) Only S&G -AUC with different num of Gaussians-
-%     [tff_pre(j), tff_rec(j), tff_f1(i,k,j)] = StaufferGrimson(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
+%     [tff_pre(j), tff_rec(j), tff_f1(j)] = StaufferGrimson(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
     %(TASK 0+1) S&G + fill holes -AUC with different num of Gaussians-
-%     [tff_pre(j), tff_rec(j), tff_f1(1,1,j)] = W3Task1(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
+%     [tff_pre(j), tff_rec(j), tff_f1(j)] = W3Task1(DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians(j), minBkgRatio, learningRate);
 end
 
  %(TASK 0+1+2) S&G + fill holes + area filtering -AUC with different num of minPixels pixels per conected component
  numGaussians = 3;
  minPixels = 2:2:20;
 for j=1:numel(minPixels)
-    [tff_pre(j), tff_rec(j), tff_f1(1,1,j)] = W3Task2(minPixels(j), DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians, minBkgRatio, learningRate);
+    [tff_pre(j), tff_rec(j), tff_f1(j)] = W3Task2(minPixels(j), DirDS, DirGT, DirOUT, seqIni, seqFini, numGaussians, minBkgRatio, learningRate);
 end
 
 
 
-%% Find the maximum f-1 score configuration
-[i,k,j] = find(tff_f1==max(max(max(tff_f1))));
-msg = sprintf('Best F1 score : %.3f (#Gauss = %d , MinBackgroundRatio = %.3f, LeraningRate = %.3f', tff_f1(i,k,j), numGaussians(j), minBkgRatio(i), learningRate(k));
-disp(msg);
-
+% AUC
 %Compute area under the Precicion-Recall Curve
-AUCTraffic = trapz(pre,rec);
+tff_AUC = trapz(tff_pre,tff_rec);
 
 %% Plot results
-
-
-% Indices of the best results
-% [i_hw,k_hw,j_hw] = find(hw_f1==max(max(max(hw_f1))));
-% [i_fl,k_fl,j_fl] = find(fall_f1==max(max(max(fall_f1))));
-% [i_tf,k_tf,j_tf] = find(tff_f1==max(max(max(tff_f1))));
-% 
-% hw_legend = sprintf('Highway (MinimunBackgroundRate=%.1f, LearningRate=%.3f)', minBkgRatio(i_hw), learningRate(k_hw));
-% fl_legend = sprintf('Fall (MinimunBackgroundRate=%.1f, LearningRate=%.3f)', minBkgRatio(i_fl), learningRate(k_fl));
-% tf_legend = sprintf('Traffic (MinimunBackgroundRate=%.1f, LearningRate=%.3f)', minBkgRatio(i_tf), learningRate(k_tf));
-% 
-% plot(numGaussians, reshape(hw_f1(i_hw,k_hw,:),1,4), '-bx', numGaussians, reshape(fall_f1(i_fl,k_fl,:),1,4), '-g^', numGaussians, reshape(tff_f1(i_tf,k_tf,:),1,4), '-ro');
-% title('Number of Gaussians');
-% legend(hw_legend,fl_legend,tf_legend);
-% xlabel('#Gaussians'); ylabel('F1-Score');
-% axis([3 6 0.3 1]);
 
 %Precision vs recall
 figure;
@@ -153,8 +125,10 @@ legend(['highway', 'fall', 'traffic']);
 xlabel('Recall');
 ylabel('Precision');
 
-%Display AUC
-hw_AUC
-fall_AUC
-tff_AUC
+%Plot AUC
+AUCS = [hw_AUC, fall_AUC, tff_AUC];
+bar(AUCS);
+legend = (['highway', 'fall', 'traffic']);
+ylabel('AUC');
+
 
