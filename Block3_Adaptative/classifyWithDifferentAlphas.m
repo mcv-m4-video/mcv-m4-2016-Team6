e8df6861d0383073696f82d4524b1177d8aa5e70
gombru <raulgombru@gmail.com>
2016-01-12 12:18:54 +0100

    Correction

--- Block3_Adaptative/classifyWithDifferentAlphas.m
+++ Block3_Adaptative/classifyWithDifferentAlphas.m
@@ -1,21 +1,18 @@
 function classifyWithDifferentAlphas( alpha, bestrho, images, filenames, imagesID, numImages, means, variances, sigmas )
 
-%Save initial values
-initialMeans = means;
-initialVariances = variances;
 
 for thIndex=1:length(alpha)  % index goes from 1 to length of alpha (11) bc we want to go through each possible alpha
     
-    %Reset values
-    means = initialMeans;
-    variances = initialVariances;
+    curMeans = means;
+    curVariances = variances;
     
     %Classify pixels
     disp(['Alpha:  ' num2str(thIndex)]);
     curAlpha = alpha(thIndex);  % counter for current alpha we're on
     [width,height] = size(images{1}); % get width and height of images
     
-    for i=1:numImages   %For each image
+    for i=1:numImages   %For each image        
+        
         disp(['Image ' num2str(i) ' of ' num2str(numImages)])
         gt_name = strcat('gt', filenames{i}(3:8), '.png');  % get name of ground truth img
 %         importfile(strcat(imagesID, '/groundtruth/', gt_name)); % load gt img
@@ -30,17 +27,13 @@ for thIndex=1:length(alpha)  % index goes from 1 to length of alpha (11) bc we w
                 else
                     curImage(m,n) = 0;    %pixel is Background
                     
-                    means_update(m,n) = bestrho*curImage(m,n)+(1-bestrho)*means(m,n); % update means
-                    variances_update(m,n) = bestrho*power(curImage(m,n)-means(m,n),2) + ...
+                    curMeans(m,n) = bestrho*curImage(m,n)+(1-bestrho)*means(m,n); % update means
+                    curVariances(m,n) = bestrho*power(curImage(m,n)-means(m,n),2) + ...
                                             (1-bestrho)*power(variances(m,n),2); % update variances
                 end
             end 
             mask_images{i} = curImage;  %Save the image mask
         end     
-        
-        %Set updated values
-        means = means_update;
-        variances = variances_update;
         
     end
     filename = strcat(imagesID, '/', imagesID, '-alpha-', num2str(thIndex), '.mat');
